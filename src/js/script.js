1bd3c559a89d35e125ad34bfa937cde87c4b01c4
joaovitor <carrera93jv@gmail.com>
2022-08-08 16:00:09 -0300

    alterações no css

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,60 @@
-const ul = document.querySelector("ul")
-let total = document.querySelector(".total")
+const ul = document.querySelector(".containerListaProdutos ul")
+
+const carrinhoCompras = []
 
 function listarProdutos(array){
     array.forEach(produto =>{ 
        let cards = criarCards(produto)
        ul.append(cards)
     })
-    let valorTotal = somarProdutos(array)
-    total.innerHTML = ""
-    total.append(`R$ ${valorTotal.toFixed(2)}`) 
 }
 listarProdutos(produtos)
 
 function criarCards(produto){
+    
     const li = document.createElement("li")
     const img = document.createElement("img")
     const h3 = document.createElement("h3")
     const span = document.createElement("span")
-    const p = document.createElement("p")
+    const olNutrientes = document.createElement("ol")
 
+    produto.componentes.forEach(elemento => {
+        const li = document.createElement("li")
+            li.innerText = elemento 
+            olNutrientes.append(li)
+    })
+  
+    const divPrecoBotao = document.createElement("div")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    
     img.src = produto.img
     img.alt = ` Imagem ${produto.nome}`
     h3.innerText = produto.nome
-    span.innerText = produto.secao 
-    p.innerText = `R$ ${produto.preco.toFixed(2)}`
+    span.innerText = produto.secao
+    divPrecoBotao.className = "divPrecoBotao"
 
-    li.append(img, h3, span, p)
+    let precoConvertido = parseInt(produto.preco)
+    p.innerText = `R$ ${precoConvertido.toFixed(2)}`
+    button.innerText = "Comprar"
+    button.id = "botaoComprar"
+
+    button.addEventListener("click",() => {
+        let produtoId = produto.id
+        let produtoClicado = produtos.find((elemento) =>{
+            if(produtoId === elemento.id){
+                return elemento
+            }
+        })
+        adicionarCarrinho(produtoClicado)
+    })
+
+    divPrecoBotao.append(p, button)
+    li.append(img, h3, span, olNutrientes, divPrecoBotao)
     
     return li
 }
+
 
 const botaoTodos = document.getElementsByClassName("estiloGeralBotoes estiloGeralBotoes--filter")[0]
 const botaoHortifruti = document.getElementsByClassName("estiloGeralBotoes estiloGeralBotoes--filter")[1]
@@ -43,30 +69,27 @@ function filtrarProdutos(){
     })
     botaoHortifruti.addEventListener("click", function(){
         ul.innerHTML = ""
-        const hortifruti = []
-        produtos.filter(produto => {
+         const hortifruti = produtos.filter(produto => {
             if(produto.secao === "Hortifruti"){
-                hortifruti.push(produto)               
+                return produto               
             }
         }) 
         listarProdutos(hortifruti) 
     })
     botaoPanificadora.addEventListener("click", function(){
         ul.innerHTML = ""
-        const panificadora = []
-        produtos.filter(produto => {
+         const panificadora = produtos.filter(produto => {
             if(produto.secao === "Panificadora"){
-                panificadora.push(produto)
+                return produto 
             }
         })
         listarProdutos(panificadora)
     })
     botaoLaticinios.addEventListener("click", function(){
         ul.innerHTML = ""
-        const laticinios = []
-        produtos.filter(produto => {
+        const laticinios = produtos.filter(produto => {
             if(produto.secao === "Laticínio"){
-                laticinios.push(produto)
+                return produto
             }
         })
         listarProdutos(laticinios)
@@ -98,7 +121,8 @@ pesquisaProdutos()
 function somarProdutos(array){
     let valores = []
     array.forEach(produto =>{
-        valores.push(produto.preco)
+        let precoConvertido = parseInt(produto.preco)
+        valores.push(precoConvertido)
     })
     return soma(valores)
 }
@@ -111,9 +135,78 @@ function soma(array){
      return soma  
 }
 
+const ulCarrinho = document.querySelector(".carrinho ul")
+const quantidade = document.querySelector(".numeroQuantidade")
+const total = document.querySelector(".valorTotal")
+
+function adicionarCarrinho(produtoAdd){
+    carrinhoCompras.push(produtoAdd)
+    listarProdutosCarrinho(carrinhoCompras)
+}
+
+function listarProdutosCarrinho(array){
+    ulCarrinho.innerHTML =""
+    array.forEach((produto) =>{
+        let miniCards = criarCardCarrinho(produto)
+        ulCarrinho.append(miniCards)
+    })
+ 
+    let valorTotal = somarProdutos(carrinhoCompras)
+    total.innerHTML = ""
+    total.append(`R$ ${valorTotal.toFixed(2)}`)
+    quantidade.innerHTML = ""
+    quantidade.append(carrinhoCompras.length)
+
+    if(carrinhoCompras.length === 0){
+        ulCarrinho.insertAdjacentHTML("afterbegin",`
+        <p class ="adicioneItens">Por enquanto não temos produtos no carrinho</p>
+        `)
+    }
+}
+
+function criarCardCarrinho(produto){
+    const li = document.createElement("li")
+    const img = document.createElement("img")
+    const spanContainer = document.createElement("span")
+    const h3 = document.createElement("h3")
+    const span = document.createElement("span")
+    const p = document.createElement("p")
+    const imgLixo = document.createElement("img")
+    const button = document.createElement("button")
+
+    button.addEventListener("click", ()=>{
+        removerProdutoCarrinho(produto)
+    })
+    
+    li.className = "cardCarrinho"
+    img.src = produto.img
+    img.alt = ` Imagem ${produto.nome}`
+    spanContainer.className = "containerH3SpanP"
+    h3.innerText = produto.nome
+    span.innerText = produto.secao
+    let precoConvertido = parseInt(produto.preco)
+    p.innerText = `R$ ${precoConvertido.toFixed(2)}`
+    imgLixo.src = "./src/img/lixeira.png"
+
+    spanContainer.append(h3, span, p)
+    button.append(imgLixo)
+    li.append(img, spanContainer, button)
+
+    return li
+}
+
+function removerProdutoCarrinho(produto) {
+    let index = carrinhoCompras.indexOf(produto)
+    carrinhoCompras.splice(index,1)
+    listarProdutosCarrinho(carrinhoCompras) 
+}
 
 
-   
+
+
+
+
+
 
 
 
